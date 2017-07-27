a6f4964827d17b3fbf923cffc37e3aed7ce5af3f
Shawn <sd19surf@gmail.com>
2017-07-27 18:35:13 -0400

    added picture.php form to upload avatar

---
+++ picture.php
@@ -0,0 +1,17 @@
+<?php
+
+  $page_title = "Upload";  # changes some of <title> in header.php to 'Upload'
+  include("includes/header.php");
+
+?>
+
+<main>
+<h1> Upload avatar for map display </h1>
+ <form action="includes/upload.php" method="post" enctype="multipart/form-data">
+	Select image to upload:
+	<input type="file" name="fileToUpload" id="fileToUpload">
+	<input type="submit" value="Upload Image" name="submit">
+</form>
+</main>
+
+<?php include("includes/footer.php"); ?>
