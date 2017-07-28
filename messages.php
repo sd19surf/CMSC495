eafc068dd9cb437b7347278ce98d71d72c87ff2c
Shawn <sd19surf@gmail.com>
2017-07-28 17:07:47 -0400

    added insert logic

--- messages.php
+++ messages.php
@@ -3,11 +3,21 @@
 // write a method to post the chat message to the database
 // capture the geolocation from the browser, userid and the message.
 
+include('session.php');
+
+$value1 = $_SESSION['UserId'];
+$value2 = $_POST['lat'];
+$value3 = $_POST['lon'];
+$value4 = $_POST['message'];
+$value5 = "CURRENT_TIMESTAMP";
 
 
+	$sql = "INSERT INTO messages (UserId, Lat, Lon, Messages, timeposted) VALUES ('$value1', '$value2', 
+     '$value3', '$value4', '$value5')";
 
+	$query = $conn->query($sql); 
 
-
+	$close = $conn->close();
 
 
 ?>
