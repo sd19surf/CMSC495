c6a795f88437ec5fa738d6d60a7b66e994b648d7
Shawn <sd19surf@gmail.com>
2017-07-29 19:18:27 -0400

    fixed JSON format return

---
+++ includes/retrieve.php
@@ -0,0 +1,24 @@
+<?php
+
+include('session.php');
+	// retrieve messages query will have to retrieve avatar image as well.
+	// so query both tables and match userid to userid and get icon
+
+	// tune this query to 
+  $query = $conn->query("SELECT u.Icon, u.username , m.messages, m.lat, m.lon, m.timeposted FROM user u, messages m WHERE u.UserId=m.UserId");
+
+	//return as JSON and provide to leaflet and process there?
+
+
+	//test script
+
+ $rows = array();
+   while ($r = mysqli_fetch_assoc($query)) {
+     $rows[] = $r;
+   }
+
+ print json_encode($rows);
+
+ $close = $conn->close();
+
+?>
