14f9ea99c9af23868ea85904b6d70a63d29f3757
kdeak1210 <kdeak1210@gmail.com>
2017-07-30 15:20:13 -0400

    Updated map page
    Added a 'placeholder' area for the map to go, added the messages form (textarea and submit button)

--- map.php
+++ map.php
@@ -6,7 +6,29 @@
 ?>
 
 <main>
-  <h1>Content area for map view</h1>
+  <br><br>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7" id="map">
+        <!-- Map Area Here -->
+        <h1>map</h1>
+      </div>
+      <div class="col-md-4 col-md-offset-1">
+
+        <!-- textArea here -->
+        <form class="" action="" method="">
+          <div class="form-group">
+            <label for="message-area">Enter a message below:</label>
+            <textarea class="form-control" rows="8" id="message-area"></textarea>
+          </div>
+          <input type="submit" name="button" class="btn btn-primary btn-lg"></button>
+        </form>
+
+
+      </div>
+    </div>
+
+  </div>
 </main>
 
 <?php include("includes/footer.php"); ?>
