2ea2590f8ebb2a594520baa87fbdf46873f028b5
kdeak1210 <kdeak1210@gmail.com>
2017-08-04 13:55:53 -0400

    Styling for Avatar Upload page
    Added Label below upload button to show selected file. TODO: Disable the upload button until an image has been selected

--- picture.php
+++ picture.php
@@ -6,12 +6,27 @@
 ?>
 
 <main>
-<h1> Upload avatar for map display </h1>
- <form action="includes/upload.php" method="post" enctype="multipart/form-data">
-	  Select image to upload:
-  	<input type="file" name="fileToUpload" id="fileToUpload">
-  	<input type="submit" value="Upload Image" name="submit">
-  </form>
+  <div class="container">
+    <div class="row">
+      <br><br>
+      <h1 class="text-center"> Upload avatar for map display </h1>
+      <br><br>
+      <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 text-center">
+        <form action="includes/upload.php" method="post" enctype="multipart/form-data">
+
+          <p>Select image to upload from your local files</p><br>
+          <label class="btn btn-warning btn-lg btn-block" for="fileToUpload">
+            <input type="file" name="fileToUpload" id="fileToUpload" style="display:none"
+            onchange="$('#file-info').html(this.files[0].name)">Browse Images
+          </label>
+          <br>
+          <input type="submit" class="btn btn-success btn-lg btn-block" value="Upload Avatar" name="submit">
+          <br>
+          <p>File Selected: <span class="label label-default label-md" id="file-info">None</span></p>
+        </form>
+      </div>
+    </div>
+  </div>
 </main>
 
 <?php include("includes/footer.php"); ?>
